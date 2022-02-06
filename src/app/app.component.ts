12907d6ce0c9aa184ebfcdd6dbc4ea627b0af930
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-06 17:54:16 +0300

    Add the Qoute class that is the blueprint for the creation of quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Quote } from './quote';
+
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,9 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quote_vote';
+  quotes: Quote[] = [
+    {id: 1, name:'The purpose of our lives is to be happy'},
+    {id: 2, name:'Many of failures in life are people who did not realise how close they were to success when they gave up'},
+    {id: 3, name:'Not how long but how well you have lived is the thing'}
+  ];
 }
