1ba73c4cad7c6594261beabcbf8ae604b5136473
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-07 04:22:05 +0300

    Add the functionality to delete a quote from the quote array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,18 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    {id: 1, name:'The purpose of our lives is to be happy', author:'Dalai Lama'},
-    {id: 2, name:'Many of failures in life are people who did not realise how close they were to success when they gave up', author:'Thomas Edison'},
-    {id: 3, name:'Not how long but how well you have lived is the thing', author:'Seneca'}
+    new Quote (1, 'The purpose of our lives is to be happy', 'Dalai Lama'),
+    new Quote (2, 'Many of failures in life are people who did not realise how close they were to success when they gave up', 'Thomas Edison'),
+    new Quote (3, 'Not how long but how well you have lived is the thing', 'Seneca')
   ];
+
+  toggleDetails(index: number) {
+    this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
+  }
+
+  deleteQuote(isComplete: any, index: number) {
+    this.quotes.splice(index, 1);
+  }
 
   constructor() { }
 
